b1d7bcecca1e1c98b3f72b15fc60df6e1ed5c9be
BarryLhm <BarryLhm@outlook.com>
2025-10-19 00:00:05 +0800

    remake :)

--- eSpeak/gen.sh
+++ eSpeak/gen.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 dir="$(dirname "$0")"
-espeak -P 0 -p 99 -v Japanese -s 100 -f "$dir/reclist.txt" -g 5 -w "$dir/release/eSpeak/eSpeak_single/all.wav"
-espeak -P 0 -p 99 -v Japanese -s 100 -f "$dir/reclist_append.txt" -g 5 -w "$dir/release/eSpeak/eSpeak_single/append.wav"
+
+for i in $(cat "$dir/reclist.txt")
+do	echo "$i"
+	espeak -P 0 -p 99 -v Japanese -s 100 -g 0 -w "$dir/release/eSpeak/eSpeak_single/$i.wav" "$i"
+done
