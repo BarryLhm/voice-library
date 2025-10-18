ca82f0a896db5974d7d6fedbda59095fef1a7af4
BarryLhm <BarryLhm@outlook.com>
2025-10-18 19:46:29 +0800

    append うぉ because を pronounces as お

--- eSpeak/gen.sh
+++ eSpeak/gen.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 dir="$(dirname "$0")"
 espeak -P 0 -p 99 -v Japanese -s 100 -f "$dir/reclist.txt" -g 5 -w "$dir/release/eSpeak/eSpeak_single/all.wav"
+espeak -P 0 -p 99 -v Japanese -s 100 -f "$dir/reclist_append.txt" -g 5 -w "$dir/release/eSpeak/eSpeak_single/append.wav"
