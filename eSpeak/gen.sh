ba59a3889965c66a84dda97070c19dcc9982d452
BarryLhm <BarryLhm@outlook.com>
2025-10-19 11:48:15 +0800

    espeak: convert to 44100hz

--- eSpeak/gen.sh
+++ eSpeak/gen.sh
@@ -3,5 +3,10 @@ dir="$(dirname "$0")"
 
 for i in $(cat "$dir/reclist.txt")
 do	echo "$i"
-	espeak -P 0 -p 99 -v Japanese -s 100 -g 0 -w "$dir/release/eSpeak/eSpeak_single/$i.wav" "$i"
+	espeak -P 0 -p 99 -v Japanese -s 100 -g 0 -w "$dir/temp/$i.wav" "$i"
+done
+
+for i in "$dir/temp/"*.wav
+do	ffmpeg -i "$i" -ar 44100 -y "$dir/release/eSpeak/eSpeak_single/${i##*/}"
+	rm "$i"
 done
